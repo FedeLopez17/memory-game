ec4006d6e5c079e6f76cf8251d15f7fa14c07b20
FedeLopez17 <fedelopez1712@gmail.com>
2024-02-06 19:45:27 -0300

    Feat - Add necessary styling values in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,31 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        loading: "url(./src/assets/images/letter-background.webp)",
+        "menu-animated":
+          "url(./src/assets/images/hogwarts-express-animated.webp)",
+        "menu-firt-frame":
+          "url(./src/assets/images/hogwarts-express-first-frame.webp)",
+        game: "url(./src/assets/images/hogwarts-background.webp)",
+        victory: "url(./src/assets/images/hogwarts-victory-background.webp)",
+        defeat: "url(./src/assets/images/hogwarts-defeat-background.webp)",
+      },
+      fontFamily: {
+        oswald: "Oswald, sans-serif",
+        roboto: "Roboto, sans-serif",
+      },
+      keyframes: {
+        "fade-in": {
+          "0%": { opacity: 0 },
+          "100%": { opacity: 1 },
+        },
+      },
+      animation: {
+        "fade-in": "fade-in 1.5s ease-in forwards",
+      },
+    },
   },
-  plugins: [],
+  plugins: [require("tailwindcss-3d")({ legacy: true })],
 };
